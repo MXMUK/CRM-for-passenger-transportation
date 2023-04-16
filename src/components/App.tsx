4fdff5c8aeb96daa784fb6f4676ac8392020e56c
Roman Maksymuk <romamaksymuk@gmail.com>
2023-04-16 12:20:51 +0300

    configuration manegment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,31 @@
-function App() {
+import type { FC } from 'react';
+
+const App: FC = () => {
   return (
     <div className="App">
-      test
+      <div className="jumbotron text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div className="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div className="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/routes/constants.ts
@@ -0,0 +1,11 @@
+export const ADMIN_ROOT = '/';
+export const PASSENGER = `passenger`;
+export const DRIVER = `driver`;
+export const MANAGER = 'manager';
+
+export const ADMIN = {
+	PATHS: {
+		ROOT: ADMIN_ROOT,
+		LOGIN: `/login`,
+	},
+};
